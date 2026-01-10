54d5fa7b8fb6123ec43f4f77ba930e3c2cbdb8c9
AITechieAbhinav <abhinavurfriend@gmail.com>
2026-01-10 13:32:16 +0530

    Initial Commit 2

--- AIToolsTest.py
+++ AIToolsTest.py
@@ -1,81 +1,55 @@
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_huggingface.embeddings import HuggingFaceEmbeddings
-from langchain_chroma import Chroma
-import chromadb
-import tqdm
-
-
 import streamlit as st
-from transformers import pipeline
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.llms import Ollama
 
-# Use a pipeline as a high-level helper
-from transformers import pipeline
+pdf_file = st.file_uploader("Upload your file", type ="pdf")
 
-st.set_page_config(page_title="AI Tools by Abhinav Gupta", layout="centered")
+#extract text
 
-st.title("📝 AI Tools by Abhinav Gupta")
+text=""
 
-tab_titles = ["Text Summarizer","Semantic Search"]
+if pdf_file is not None:
+    reader = Pdfreader(pdf_file)
+    
+    for page_text in reader.pages:
+        if page_text:
+            text+=page_text.extract_text()
 
-tab1, tab2 = st.tabs(tab_titles)
+#split text into chunks
 
-with tab1:
+text_spliter = CharacterTextSplitter(
+    separator="\n",
+    chunk_size=1000,
+    chunk_overlap=200,
+    length_function= len
+)
 
-	text = st.text_input("Paste text below to Summarize")
+#Save chunks
+data = text_spliter.split_text(text)
 
-	if st.button("Submit") :
+if len(data) == 0:
+    st.stop()
 
-		summarizer = pipeline("summarization", model="pszemraj/long-t5-tglobal-base-16384-book-summary")
-		sm_txt = summarizer(text)
-		st.markdown(sm_txt)
+#Create embeedings
 
-########################################################
+embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
 
-with tab2:
+database = FAISS.from_texts(data,embeddings)
 
-  uploaded_file = st.file_uploader("Upload a PDF document", type=["pdf"])
+#User input
 
-  if uploaded_file is not None:
-    # Step 2: Save to a temporary file to provide a path for the loader
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
-        tmp_file.write(uploaded_file.getvalue())
-        tmp_path = tmp_file.name
+u_input = st.text_input("Please ask questions about PDF file")
 
-    # Step 3: Load and split the document
-    loader = PyPDFLoader(tmp_path)
-    documents = loader.load()
+llm = Ollama(model="llama3")
 
-  text_splitter = RecursiveCharacterTextSplitter(
-  chunk_size=500, #Number of tokens to keep in a chunk
-  chunk_overlap=16 #Number of overlap tokens
-  )
-  
-  data_chunks = uploaded_file.load_and_split(text_splitter)
+if u_input :
+    search_result = databse.similarity_search(user_input)
+    chain = load_qa_chain(llm,chain_type="stuff",verbose=True)
 
-  embedding_model = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+    with get_openai_callback() as cb:
+        reponse = chain.run(input_documents= search_result, question=u_input)
+        print(cb)
 
-  chromadb_client = chromadb.PersistentClient(path='./chroma_db')
-
- # Instantiate a Chroma vector store to store and retrieve document embeddings
-  vector_store = Chroma(
-    embedding_function=embedding_model,
-    client=chromadb_client,
-    collection_name='maxbupa_collection',
-  )
-
-
-  for i in tqdm.tqdm(range(len(data_chunks))):
-      vector_store.add_documents(documents=[data_chunks[i]])
-
-  vector_store._collection.count()
-  user_query = "PREFACE"
-
-#To retrieve relevant chunks for the user_query from the vectore store , we need to first create retriever interface to vectore store
-  retriever = vector_store.as_retriever(
-  search_type='similarity', #Metric to use to find relevant documents
-  search_kwargs={'k': 5} #Number of chunks to return
-  )
-
-  relevant_chunks = retriever.invoke(user_query)
-  relevant_chunks[1].page_content
+    st.write(response)
