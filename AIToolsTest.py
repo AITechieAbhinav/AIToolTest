412b006dba5229e672b179751fc364fc6c899702
AITechieAbhinav <abhinavurfriend@gmail.com>
2026-01-10 14:50:56 +0530

    Update AIToolsTest.py

--- AIToolsTest.py
+++ AIToolsTest.py
@@ -46,7 +46,7 @@ database = FAISS.from_texts(data,embeddings)
 
 u_input = st.text_input("Please ask questions about PDF file")
 
-llm = HuggingFaceEndPoint(model="meta-llama/Llama-3.1-8B-Instruct", token="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")
+llm = HuggingFaceEndPoint(repo_id="meta-llama/Llama-3.1-8B-Instruct", token="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")
 
 if u_input :
     search_result = database.similarity_search(u_input)
