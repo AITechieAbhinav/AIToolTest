4d5d0eb7e54599245e9fa29f02c99e90518cece1
AITechieAbhinav <abhinavurfriend@gmail.com>
2026-01-10 15:29:33 +0530

    Update AIToolsTest.py

--- AIToolsTest.py
+++ AIToolsTest.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_huggingface.embeddings import HuggingFaceEmbeddings
-from langchain.llms import HuggingFaceHub
+from langchain_huggingface import HuggingFaceEndpoint
 from langchain_community.vectorstores import FAISS
 from langchain_classic.chains.question_answering import load_qa_chain
 from langchain_community.callbacks import get_openai_callback
@@ -45,7 +45,7 @@ database = FAISS.from_texts(data,embeddings)
 
 u_input = st.text_input("Please ask questions about PDF file")
 
-llm = HuggingFaceHub(repo_id="meta-llama/Llama-3.1-8B-Instruct", token="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")
+llm = HuggingFaceEndpoint(repo_id="meta-llama/Llama-3.1-8B-Instruct", token="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")
 
 if u_input :
     search_result = database.similarity_search(u_input)
