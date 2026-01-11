69ec71c297c5401b6b473f8e7fa0ea853e16b237
AITechieAbhinav <abhinavurfriend@gmail.com>
2026-01-11 15:19:56 +0530

    Update AIToolsTest.py

--- AIToolsTest.py
+++ AIToolsTest.py
@@ -1,18 +1,13 @@
 import streamlit as st
 import torch
-from diffusers import StableDiffusionPipeline 
+from diffusers import DiffusionPipeline 
 
 api_key = st.secrets['api_key']
 
 st.set_page_config(page_title="AI Image Generator", layout="centered")
 st.title("🎨 AI Image Generator (Stable Diffusion)")
 
-device = "cpu"
-
-modelid = "CompVis/stable-diffusion-v1-4"
-device = "cuda"
-pipe = StableDiffusionPipeline.from_pretrained(modelid, revision="fp16", torch_dtype=torch.float32, token=api_key) 
-pipe.to(device) 
+pipe = DiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-2", dtype=torch.bfloat16, device_map="cuda")
 
 prompt = st.text_area("Enter your prompt",value=
                       """dreamlikeart, a grungy woman with rainbow hair, travelling between dimensions,dynamic pose, happy, soft eyes and narrow chin, 
