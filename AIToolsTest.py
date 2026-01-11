fd57d96c6f8cf0787865f6cdd2724900845e24d5
AITechieAbhinav <abhinavurfriend@gmail.com>
2026-01-11 22:28:57 +0530

    Update AIToolsTest.py

--- AIToolsTest.py
+++ AIToolsTest.py
@@ -1,6 +1,10 @@
+from transformers import T5Tokenizer, T5ForConditionalGeneration
 import torch
 import streamlit as st
-from transformers import pipeline
+
+model_name = "t5-base" 
+tokenizer = T5Tokenizer.from_pretrained(model_name)
+model = T5ForConditionalGeneration.from_pretrained(model_name)
 
 st.set_page_config(page_title="AI Tools by Abhinav Gupta", layout="centered")
 
@@ -8,12 +12,14 @@ st.title("📝 AI Tools by Abhinav Gupta")
 
 input_text = st.text_input("Paste text below to Summarize")
 
-with tab1:
+if st.button("Submit"):
 
-	text = st.text_input("Paste text below to Summarize")
-
-	if st.button("Submit") :
-
-		summarizer = pipeline("summarization", model="google/pegasus-cnn_dailymail")
-		sm_txt = summarizer(text)
-		st.markdown(sm_txt)
+inputs = tokenizer(input_text, return_tensors="pt")
+summary_ids = model.generate(
+inputs.input_ids,
+max_length=200,
+num_beams=5,
+early_stopping=True
+)
+output_text = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+st.markdown(output_text)
