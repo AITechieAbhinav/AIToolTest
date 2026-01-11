7ebbd1f833e61452ba6aa2c509380b2867db318d
AITechieAbhinav <abhinavurfriend@gmail.com>
2026-01-11 13:49:15 +0530

    Update AIToolsTest.py

--- AIToolsTest.py
+++ AIToolsTest.py
@@ -7,11 +7,11 @@ api_key = st.secrets['api_key']
 st.set_page_config(page_title="AI Image Generator", layout="centered")
 st.title("🎨 AI Image Generator (Stable Diffusion)")
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
+device = "cpu"
 
 modelid = "CompVis/stable-diffusion-v1-4"
 device = "cuda"
-pipe = StableDiffusionPipeline.from_pretrained(modelid, revision="fp16", torch_dtype=torch.float16, use_auth_token=api_key) 
+pipe = StableDiffusionPipeline.from_pretrained(modelid, revision="fp16", torch_dtype=torch.float32, use_auth_token=api_key) 
 pipe.to(device) 
 
 prompt = st.text_area("Enter your prompt",value=
