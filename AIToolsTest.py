d233449e91edc8bc0ad7fce24549faf7325a0b58
AITechieAbhinav <abhinavurfriend@gmail.com>
2026-01-11 22:24:38 +0530

    Update AIToolsTest.py

--- AIToolsTest.py
+++ AIToolsTest.py
@@ -1,15 +1,6 @@
-#from transformers import T5Tokenizer, T5ForConditionalGeneration
 import torch
 import streamlit as st
-
-model_name = "google/pegasus-cnn_dailymail" 
-#tokenizer = T5Tokenizer.from_pretrained(model_name)
-#model = T5ForConditionalGeneration.from_pretrained(model_name)
-
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-
-tokenizer = AutoTokenizer.from_pretrained("google/pegasus-cnn_dailymail")
-model = AutoModelForSeq2SeqLM.from_pretrained("google/pegasus-cnn_dailymail")
+from transformers import pipeline
 
 st.set_page_config(page_title="AI Tools by Abhinav Gupta", layout="centered")
 
@@ -17,14 +8,12 @@ st.title("📝 AI Tools by Abhinav Gupta")
 
 input_text = st.text_input("Paste text below to Summarize")
 
-if st.button("Submit"):
+with tab1:
 
-    inputs = tokenizer(input_text, return_tensors="pt")
-    summary_ids = model.generate(
-        inputs.input_ids,
-        max_length=200,
-        num_beams=5,
-        early_stopping=True
-    )
-    output_text = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
-    st.markdown(output_text)
+	text = st.text_input("Paste text below to Summarize")
+
+	if st.button("Submit") :
+
+		summarizer = pipeline("summarization", model="google/pegasus-cnn_dailymail")
+		sm_txt = summarizer(text)
+		st.markdown(sm_txt)
