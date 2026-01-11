c58a062b55a0e853f8ee13d20360d0c722f4b0ef
AITechieAbhinav <abhinavurfriend@gmail.com>
2026-01-11 18:58:45 +0530

    Update AIToolsTest.py

--- AIToolsTest.py
+++ AIToolsTest.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from transformers import SpeechT5Processor, SpeechT5ForTextToSpeech, SpeechT5HifiGan
+from huggingface_hub import hf_hub_download
 import soundfile as sf
 import torch
 import tempfile
