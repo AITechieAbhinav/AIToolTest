27d673ed5f0e56c76deee1812d92ff16e4b5f81b
AITechieAbhinav <abhinavurfriend@gmail.com>
2026-01-11 22:06:57 +0530

    Update AIToolsTest.py

--- AIToolsTest.py
+++ AIToolsTest.py
@@ -12,9 +12,14 @@ st.title("📝 AI Tools by Abhinav Gupta")
 
 input_text = st.text_input("Paste text below to Summarize")
 
-if st.button("Submit") :
+if st.button("Submit"):
 
-		inputs = tokenizer(input_text, return_tensors="pt")
-	    summary_ids = model.generate(inputs.input_ids, max_length=50, num_beams=5, early_stopping=True) 
-        output_text = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
-		st.markdown(output_text)
+    inputs = tokenizer(input_text, return_tensors="pt")
+    summary_ids = model.generate(
+        inputs.input_ids,
+        max_length=50,
+        num_beams=5,
+        early_stopping=True
+    )
+    output_text = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    st.markdown(output_text)
